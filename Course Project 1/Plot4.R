ad4aabba909206f5ba8ab92a6bb99fe51b0a9fb7
Guillermo Pereira <guillermopereira495@gmail.com>
2018-08-29 01:05:54 -0300

    Coursera 'Exploratory Data Analysis' course - Course Project 1
    The following push contains the scripts and png images requested as part of Coursera's 'Exploratory Data Analysis' course project 1 (from week 1).

---
+++ Course Project 1/Plot4.R
@@ -0,0 +1,56 @@
+# COURSERA: EXPLORATORY DATA ANALYSIS, week 1 Course Project 1
+#---------------------------------------------------------------
+# Create directory (if non-existent)
+# Download zip file
+# Unzip file in desired directory 
+
+if(!file.exists("ExploratoryDataAnalysis")) {
+        dir.create("ExploratoryDataAnalysis")
+}
+
+setwd("./ExploratoryDataAnalysis")
+
+if(!file.exists("HouseholdPowerConsumption.zip")) {
+        fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileURL, destfile = "HouseholdPowerConsumption.zip")        
+}
+
+unzip("HouseholdPowerConsumption.zip")
+
+#-----------------------------------------------------------------------------------
+# Extract rows related to dates 01/Feb/2007 - 02/Feb/2007 (i.e. 2880 records):
+
+HouseholdPowerConsumption <- read.csv("household_power_consumption.txt", sep = ";", na.strings = "?", header = F, skip = 66637, nrows = 2880)
+
+# Extract only column names from datafile (i.e. 1st row only), rename columns:
+
+ColumnNames <- read.csv("household_power_consumption.txt", sep = ";", nrows = 1, as.is = T, header = F, colClasses = "character")
+colnames(HouseholdPowerConsumption) <- ColumnNames
+
+# Change the $Date and $Time (char) variables into a single POSIXlt class DateTime variable:
+
+HouseholdPowerConsumption$Date <- paste(HouseholdPowerConsumption$Date, HouseholdPowerConsumption$Time, sep=", ")       # paste 'Date' and 'Time' columns together
+library(dplyr)  # Loading dplyr for easier manipulation (i.e. function 'rename()')
+HouseholdPowerConsumption <- rename(HouseholdPowerConsumption, DateTime = Date)
+HouseholdPowerConsumption$DateTime <- strptime(HouseholdPowerConsumption$DateTime, format = "%d/%m/%Y, %H:%M:%S")       # Changing 'DateTime' from char to POSIXlt
+HouseholdPowerConsumption$Time <- NULL   # Dropping the 'Time' variable, as it is now incorporated into 'DateTime'
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+# Create plot4
+
+png(filename = "plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2))
+with(HouseholdPowerConsumption, {
+        plot(DateTime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+        plot(DateTime, Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+        plot(DateTime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"); lines(DateTime, Sub_metering_2, col = "red"); lines(DateTime, Sub_metering_3, col = "blue"); legend("topright", bty = "n", lty = "solid", col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"));
+        plot(DateTime, Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power"); 
+})
+dev.off()
+
+# Move working directory one level up (i.e. out of the 'Exploratory Data Analysis' directory)
+setwd("..")
+
+#--------------------------------------------------------------
+# SCRIPT END
+#--------------------------------------------------------------
